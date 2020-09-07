38d2b65d319832fc4993d714c213b5bcbd08a19b
dominikwei <dominikwei@tencent.com>
2020-09-07 17:02:56 +0800

    create screenMainTool

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,5 @@
 import { app, BrowserWindow, globalShortcut } from 'electron'
-
+import screenMaintool  from '../renderer/lib/screenMainTool'
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
@@ -7,75 +7,12 @@ import { app, BrowserWindow, globalShortcut } from 'electron'
 if (process.env.NODE_ENV !== 'development') {
   global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
-
-let mainWindow
-let captureWins = []
-const os = require('os')
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
-const captureURL = winURL + '#capture'
 
-function captureWin() {
-  if (captureWins.length) {
-    return
-  }
-  const { screen } = require('electron')
+let mainWindow
 
-  let displays = screen.getAllDisplays()
-  captureWins = displays.map((display) => {
-    let captureWin = new BrowserWindow({
-      // window 使用 fullscreen,  mac 设置为 undefined, 不可为 false
-      fullscreen: os.platform() === 'win32' || undefined,
-      width: display.bounds.width,
-      height: display.bounds.height,
-      x: display.bounds.x,
-      y: display.bounds.y,
-      transparent: true,
-      frame: false,
-      // skipTaskbar: true,
-      // autoHideMenuBar: true,
-      movable: false,
-      resizable: false,
-      enableLargerThanScreen: true,
-      hasShadow: false,
-      webPreferences: {
-        nodeIntegration: true,
-        webviewTag: true,
-        webSecurity: process.env.NODE_ENV === 'production'
-      }
-    })
-    captureWin.setAlwaysOnTop(true, 'screen-saver')
-    captureWin.setVisibleOnAllWorkspaces(true)
-    captureWin.setFullScreenable(false)
-
-    captureWin.loadURL(captureURL)
-    captureWin.show()
-    let { x, y } = screen.getCursorScreenPoint()
-    if (x >= display.bounds.x && x <= display.bounds.x + display.bounds.width && y >= display.bounds.y && y <= display.bounds.y + display.bounds.height) {
-      captureWin.focus()
-    } else {
-      captureWin.blur()
-    }
-    // 调试用
-    // captureWin.openDevTools()
-
-    captureWin.on('closed', () => {
-      let index = captureWins.indexOf(captureWin)
-      if (index !== -1) {
-        captureWins.splice(index, 1)
-      }
-      captureWins.forEach(win => win.close())
-    })
-    return captureWin
-  })
-  globalShortcut.register('Esc', () => {
-    if (captureWins) {
-      captureWins.forEach(win => win.hide())
-      captureWins = []
-    }
-  })
-}
 function createWindow () {
   /**
    * Initial window options
@@ -97,7 +34,7 @@ function createWindow () {
   mainWindow.on('closed', () => {
     mainWindow = null
   })
-  captureWin()
+  screenMaintool.captureWin()
 }
 
 app.on('ready', createWindow)
